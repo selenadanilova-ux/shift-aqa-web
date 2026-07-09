2888742039bcea467cd477b4cb0b2b87949f0fbf
Елена Торгашина <selenadanilova@gmail.com>
2026-07-09 12:10:43 +0700

    Update shoppingCart.spec.ts

--- tests/shoppingCart.spec.ts
+++ tests/shoppingCart.spec.ts
@@ -1,10 +1,17 @@
 import {test, expect} from '@playwright/test'
 
-test('Корзина с товаром', async({page}) => {
-
+test('Корзина с товаром', async ({ page }) => {
     await page.goto('/catalog');
     await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
-    await page.getByTestId('catalog-add-to-cart-button-prod-001').check();
+    await page.getByTestId('catalog-add-to-cart-button-prod-001').click();
     await page.getByTestId('header-cart-button').click();
-    await expect
-})
+    await page.waitForURL('**/cart');
+    await expect.soft(page).toHaveScreenshot({
+        fullPage: true,
+        mask: [
+        page.locator('p.text-sm.font-semibold.text-primary'),
+        page.getByTestId('cart-captcha-image'),
+        page.getByTestId('cart-total-price')                       
+    ]        
+        });
+});
